d862661df46d60ebbc7a091eaccaced9b704e20b
Sayo Adegoroye <steve.legion94@gmail.com>
2024-03-18 16:19:08 +0100

    new

--- lib/action/event.actions.ts
+++ lib/action/event.actions.ts
@@ -5,6 +5,17 @@ import { handleError } from "../utils";
 import { connectToDatabase } from "../database";
 import User from "../database/models/user.model";
 import Event from "../database/models/event.model";
+import Category from "../database/models/category.model";
+
+const populateEvent = (query: any) => {
+  return query
+    .populate({
+      path: "organizer",
+      model: User,
+      select: "_id firstName lastName",
+    })
+    .populate({ path: "category", model: Category, select: "_id name" });
+};
 
 export const createEvent = async ({
   event,
@@ -31,3 +42,17 @@ export const createEvent = async ({
     handleError(error);
   }
 };
+
+export async function getEventById(eventId: string) {
+  try {
+    await connectToDatabase();
+
+    const event = await populateEvent(Event.findById(eventId));
+
+    if (!event) throw new Error("Event not found");
+
+    return JSON.parse(JSON.stringify(event));
+  } catch (error) {
+    handleError(error);
+  }
+}
