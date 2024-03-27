30f1533b7908293195e0b3b9b51cbe2297871df6
Sayo Adegoroye <steve.legion94@gmail.com>
2024-03-27 17:42:35 +0100

    new

--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -7,8 +7,10 @@ const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
     console.log("CHECKOUT");
   };
   return (
-    <form action={onCheckOut}>
-      <Button>{event.isFree ? "Get Ticket" : "Buy Ticket"}</Button>
+    <form action={onCheckOut} method="post">
+      <Button type="submit" role="link" size="lg" className="button sm:w-fit">
+        {event.isFree ? "Get Ticket" : "Buy Ticket"}
+      </Button>
     </form>
   );
 };
