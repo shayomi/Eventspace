8cf9ac7f28a8a5d806267555dd02f6c55c126c10
Sayo Adegoroye <steve.legion94@gmail.com>
2024-04-05 16:25:02 +0100

    finishing

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,12 +4,19 @@ import { Link2 } from "lucide-react";
 import Link from "next/link";
 import Collection from "@/components/shared/Collection";
 import { getAllEvents } from "@/lib/action/event.actions";
+import Search from "@/components/shared/Search";
+import { SearchParamProps } from "@/types";
+import CategoryFilter from "@/components/shared/CategoryFilter";
 
-export default async function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || "";
+  const category = (searchParams?.category as string) || "";
+
   const events = await getAllEvents({
-    query: "",
-    category: "",
-    page: 1,
+    query: searchText,
+    category,
+    page,
     limit: 6,
   });
 
@@ -47,17 +54,20 @@ export default async function Home() {
           {" "}
           Trusted by <br /> Thousands of Events
         </h2>
+
         <div className="flex w-full flex-col gap-5 md:flex-row">
-          search category
+          <Search />
+          <CategoryFilter />
         </div>
+
         <Collection
           data={events?.data}
           emptyTitle="No Events Found"
           emptyStateSubtext="Come back Later"
           collectionType="All_Events"
           limit={6}
-          page={1}
-          totalPages={2}
+          page={page}
+          totalPages={events?.totalPages}
         />
       </section>
     </>
--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,21 +1,28 @@
-import React from "react";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import { auth } from "@clerk/nextjs";
 import Collection from "@/components/shared/Collection";
+import { Button } from "@/components/ui/button";
 import { getEventsByUser } from "@/lib/action/event.actions";
 import { getOrdersByUser } from "@/lib/action/order.action";
 import { IOrder } from "@/lib/database/models/order.model";
+import { SearchParamProps } from "@/types";
+import { auth } from "@clerk/nextjs";
+import Link from "next/link";
+import React from "react";
 
-const ProfilePage = async () => {
+const ProfilePage = async ({ searchParams }: SearchParamProps) => {
   const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string;
-  const orders = await getOrdersByUser({ userId, page: 1 });
+
+  const ordersPage = Number(searchParams?.ordersPage) || 1;
+  const eventsPage = Number(searchParams?.eventsPage) || 1;
+
+  const orders = await getOrdersByUser({ userId, page: ordersPage });
+
   const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
-  const organizedEvents = await getEventsByUser({ userId, page: 1 });
+  const organizedEvents = await getEventsByUser({ userId, page: eventsPage });
+
   return (
     <>
-      {/* My tickets */}
+      {/* My Tickets */}
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <div className="wrapper flex items-center justify-center sm:justify-between">
           <h3 className="h3-bold text-center sm:text-left">My Tickets</h3>
@@ -32,7 +39,7 @@ const ProfilePage = async () => {
           emptyStateSubtext="No worries - plenty of exciting events to explore!"
           collectionType="My_Tickets"
           limit={3}
-          page={1}
+          page={ordersPage}
           urlParamName="ordersPage"
           totalPages={orders?.totalPages}
         />
@@ -55,7 +62,7 @@ const ProfilePage = async () => {
           emptyStateSubtext="Go create some now"
           collectionType="Events_Organized"
           limit={3}
-          page={1}
+          page={eventsPage}
           urlParamName="eventsPage"
           totalPages={organizedEvents?.totalPages}
         />
